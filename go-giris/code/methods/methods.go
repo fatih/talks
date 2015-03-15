77f92be4571cd9ece2af351c24221b853c984417
Fatih Arslan <ftharsln@gmail.com>
2015-03-15 11:07:15 +0200

    go-giris: add zero values and methods

---
+++ go-giris/code/methods/methods.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// START OMIT
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+	v := Vertex{3, 4}
+	fmt.Println(v.Abs())
+}
+
+// END OMIT
---
+++ go-giris/code/zero/zero.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// START OMIT
+func main() {
+	var i int
+	var f float64
+	var b bool
+	var s string
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+}
+
+// END OMIT
