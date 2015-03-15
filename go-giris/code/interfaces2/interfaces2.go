038249b0f6f5b6cbf9afea9c96e7336b7067fb18
Fatih Arslan <ftharsln@gmail.com>
2015-03-15 13:05:50 +0200

    go-giris: more interfaces examples

---
+++ go-giris/code/interfaces2/interfaces2.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type Adder interface {
+	Add(int) int
+}
+
+type Point struct {
+	X, Y int
+}
+
+func (p Point) Add(a int) int {
+	return p.X + p.Y + a
+}
+
+func (p Point) String() string {
+	return fmt.Sprintf("X: %d Y: %d Sum: %d", p.X, p.Y, p.Add(0))
+}
+
+type MyInt int
+
+func (m MyInt) Add(a int) int {
+	return int(m) + a
+}
+
+func (m MyInt) String() string {
+	return "*** " + strconv.Itoa(int(m)) + " ***"
+}
+
+// START OMIT
+func main() {
+	p := Point{3, 4}
+	m := MyInt(3)
+
+	fmt.Println(p)
+	fmt.Println(m)
+}
+
+// END OMIT
