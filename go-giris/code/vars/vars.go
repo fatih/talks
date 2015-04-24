24977600e65b6ef16a9bf0a2628becfbbbfed6e3
Fatih Arslan <ftharsln@gmail.com>
2015-04-24 21:11:36 +0300

    go-giris: show more examples of variables

--- go-giris/code/vars/vars.go
+++ go-giris/code/vars/vars.go
@@ -8,18 +8,19 @@ var sayı = 13
 const aciklama = "Örnek bir Go uygulamasi"
 
 func hesapla(x, y, z int) (int, bool) {
-	a := x + y + z
+	var a int = x + y + z
 	b := 20 < a
+
 	return a, b
 }
 
 func main() {
 	fmt.Printf("%s\n", aciklama)
 
-	var a int
-	b := 8
+	var empty int
+	number := 8
 
-	toplam, buyuk := hesapla(a, b, sayı)
+	toplam, buyuk := hesapla(empty, number, sayı)
 	fmt.Println(toplam, buyuk)
 }
 
